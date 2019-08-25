094368654d3ee9313624d861ad591823c0dd8167
draganov <draganov89@mail.bg>
2019-08-25 23:42:21 +0300

    Point class implemented, example data added

---
+++ travelingSalesman/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace travelingSalesman
+{
+    public class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public static double GetTotalDistance(Point one, Point two)
+        {
+            var a = Math.Pow(Math.Abs(one.X - two.X), 2);
+            var b = Math.Pow(Math.Abs(one.Y - two.Y), 2);
+            return Math.Sqrt(a + b);
+            
+        }
+    }
+}
---
+++ travelingSalesman/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace travelingSalesman
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var load = new Dictionary<string, Point>()
+            {
+                ["A"] = new Point(2, 2),
+                ["B"] = new Point(3, 7),
+                ["C"] = new Point(10, 4),
+            };
+        }
+    }
+}
